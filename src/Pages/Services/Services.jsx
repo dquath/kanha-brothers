3da7ab26c13e967577c6f3ede41e7f9ffed32554
Soubhagya Darshan Rout <soubhagyadarshan@gmail.com>
2023-05-27 02:07:48 +0530

    service section complete

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./Services.css";
 
 const Services = () => {
-  return <div className="services">Services</div>;
+  return <div className="services">
+    <div className="heading">
+      <h1 className="hdng">Services</h1>
+    </div>
+      <div className="btndesc">
+        <div className="description">
+          <p>Lorem ipsum dolor sit amet consectetur.
+          Amet at fermentum ut sed elit adipiscing 
+          cursus ullamcorper libero. </p>
+        </div>
+        <div className="btn">
+          <button>Explore our services</button>
+        </div>
+      </div>
+  </div>;
 };
 
 export default Services;
