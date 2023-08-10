9ea92d484d36d93e4e4d796fa469da73ba0ea3a4
Rashmi Ranjan Sahoo <rashmiranjan@tykeinvest.com>
2023-08-10 21:38:37 +0530

    Add Google Map

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,17 +1,66 @@
-import React from "react";
 import "./Contact.css";
 
+import React, { useState } from "react";
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = () => {
+    console.log(formData); // This will log the form data. Here, you can send it to a backend or perform any other action.
+
+    // Clearing the form data.
+    setFormData({
+      name: "",
+      phone: "",
+      email: "",
+      message: "",
+    });
+  };
   return (
     <div className="main">
       <div className="left-part">
         <div className="contact-heading">Send a message</div>
         <div className="form">
-          <input type="text" placeholder="Enter your name"></input>
-          <input type="text" placeholder="Enter your phone number"></input>
-          <input type="text" placeholder="Enter your email"></input>
-          <input type="text" placeholder="What’s the message?"></input>
-          <button>Send</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="phone"
+            placeholder="Enter your phone number"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Enter your email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="message"
+            placeholder="What’s the message?"
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <button onClick={handleSubmit}>Send</button>
         </div>
       </div>
       <div className="right-part">
@@ -26,7 +75,15 @@ const Contact = () => {
           <p>Burla, Odisha 768019</p>
         </div>
         <div className="map-image">
-          <img src="https://i.stack.imgur.com/B6fEt.png" alt="map"></img>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3712.791540291418!2d83.87897397526304!3d21.47669457224302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a213c30d3c00001%3A0x57a50679ae0cef7a!2sKanha%20%26%20Brothers!5e0!3m2!1sen!2sin!4v1691683540070!5m2!1sen!2sin"
+            width="223"
+            height="200"
+            style={{ border: 0 }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </div>
--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,12 +1,14 @@
-import React from "react";
 import "./Footer.css";
-import footerLogo from "../../../Assets/images/Logo-mobile.svg";
+
+import { Link } from "react-router-dom";
+import React from "react";
 import demoMap from "../../../Assets/images/Demo-map.png";
 import facebookIcon from "../../../Assets/icons/facebook-icon.svg";
+import footerLogo from "../../../Assets/images/Logo-mobile.svg";
 import instagramIcon from "../../../Assets/icons/instagram-icon.svg";
-import twitterIcon from "../../../Assets/icons/twitter-icon.svg";
 import messageIcon from "../../../Assets/icons/message-icon.svg";
 import phoneFooterIcon from "../../../Assets/icons/phone-icon-footer.svg";
+import twitterIcon from "../../../Assets/icons/twitter-icon.svg";
 
 const Footer = () => {
   return (
@@ -46,7 +48,15 @@ const Footer = () => {
                 <p>
                   Market Complex, Jyoti Vihar, <br /> Burla, Odisha 768019
                 </p>
-                <img className="footer-map" src={demoMap} alt="" />
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3712.791540291418!2d83.87897397526304!3d21.47669457224302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a213c30d3c00001%3A0x57a50679ae0cef7a!2sKanha%20%26%20Brothers!5e0!3m2!1sen!2sin!4v1691683540070!5m2!1sen!2sin"
+                  width="223"
+                  height="200"
+                  style={{ border: 0 }}
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
               </div>
             </div>
           </div>
@@ -54,7 +64,9 @@ const Footer = () => {
       </div>
       <div className="footer-bottom-section">
         <div className="copyright-text">
-          <p>© 2023 Kanha & Brothers.</p>
+          <p>
+            © 2023 <Link to={"/"}>Kanha & Brothers</Link>.
+          </p>
           <p>
             Crafted by <span>DQuath</span>
           </p>
