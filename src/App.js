ef7038a2fdde595e095ebc81b2e37921e5983f3e
Rashmi Ranjan Sahoo <rashmiranjan@tykeinvest.com>
2023-08-10 21:23:54 +0530

    solve the uRl issue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Navbar from "./components/common/Navbar/Navbar";
 import "./global.css";
-import Footer from "./components/common/Footer/Footer";
-import SingleService from "./Pages/Services/pages/Single-Service/SingleService";
-import Services from "./Pages/Services/Services";
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 // import Printing from "./Pages/Single-service/Printing";
 import Contact from "./Pages/Contact/Contact";
+import Footer from "./components/common/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import Navbar from "./components/common/Navbar/Navbar";
 import Product from "./Pages/Product/Product";
+import React from "react";
+import Services from "./Pages/Services/Services";
+import SingleService from "./Pages/Services/pages/Single-Service/SingleService";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
